2ab3762412d5a28c87f10cdea0baed58f3a0edbb
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2021-03-29 21:31:41 -0500

    Update deepgaze.py

--- deepgaze.py
+++ deepgaze.py
@@ -75,10 +75,7 @@ class FeatureExtractor(torch.nn.Module):
 
     def forward(self, x):
         self.outputs.clear()
-
-        #print(x.type(),'niii')
         x=x.type('torch.cuda.FloatTensor')
-        #print(x.type(),self.features.type,'wiii')
         self.features(x).type('torch.cuda.FloatTensor')
 
         return [self.outputs[target] for target in self.targets]
@@ -348,35 +345,9 @@ class DeepGazeIII(torch.nn.Module):
 
         readout_shape = [math.ceil(orig_shape[2] / self.downsample / self.readout_factor), math.ceil(orig_shape[3] / self.downsample / self.readout_factor)]
         x = [F.interpolate(item, readout_shape) for item in x]
-        #TEMx = [F.interpolate(item, readout_shape) for item in TEMx]
-        ##TEM=F.interpolate(TEM,size=readout_shape,mode="bilinear").cuda()
-
-        salv=[]
-        #for i in range(0,len(x)):
-            #print(x[i].shape,TEM.shape,'shapes_u')
-            #mn_vals=torch.zeros((x[0].shape[0],x[0].shape[1]+TEMx[0].shape[1],x[0].shape[2],x[0].shape[3]))
-            #for p in range(0,x[0].shape[0]):
-            #    for k in range(0,x[0].shape[1]):
-            #        #print(x[i][p,k,:,:].shape,TEM[p,k,:,:].shape)
-            #        mn_vals[p,k,:,:]=torch.mul(x[i][p,k,:,:],torch.round(TEM[p,0,:,:]))
-            #mn_vals=torch.cat((x[i][:,:,:,:],TEM[:,:,:,:]),dim=1)
-            #print(x[i][:,:,:,:].shape,TEMx[i][:,:,:,:].shape,'shapes_p')
-            #mn_vals=torch.cat((x[i][:,:,:,:],TEMx[i][:,:,:,:]),dim=1)
-            #salv.append(mn_vals)
-
-        
-        #for i in range(0,len(x)):
-        #    x[i]=x[i][:,0:300,:,:]
-        
-        #print(salv[0].shape,'shapesj')
+     
         x = torch.cat(x,dim=1)
         x = self.saliency_network(x)
-
-        #salq=torch.zeros_like(x).cuda()
-        #salq=x.detach().clone().cuda()
-        
-        #TEMd=F.interpolate(TEM,size=(x.shape[2],x.shape[3]),mode="bilinear").cuda()
-        #salq=torch.cat((salq,TEMd[:,:,:,:]),dim=1)
 
         if self.scanpath_network is not None:
             scanpath_features = encode_scanpath_features(x_hist, y_hist, size=(orig_shape[2], orig_shape[3]), device=x.device)
@@ -388,7 +359,6 @@ class DeepGazeIII(torch.nn.Module):
         x = self.fixation_selection_network((x, y))
 
         x = self.finalizer(x.type('torch.cuda.FloatTensor'), centerbias.type('torch.cuda.FloatTensor'))
-        #print(x.type(),'siii')
         
         return x
 
@@ -411,52 +381,24 @@ class DeepGaze_TEM(torch.nn.Module):
         self.included_fixations = included_fixations
 
         self.features = features
-        #self.TEM = TEM
 
         for param in self.features.parameters():
             param.requires_grad = False
         self.features.eval()
         
         self.saliency_network = saliency_network
-        torch.nn.init.xavier_normal_(self.saliency_network.conv0.weight)
-        torch.nn.init.xavier_normal_(self.saliency_network.conv1.weight)
-        torch.nn.init.xavier_normal_(self.saliency_network.conv2.weight)
-        self.saliency_network_TEM = saliency_network_TEM
-        torch.nn.init.xavier_normal_(self.saliency_network_TEM.conv0.weight)
-        torch.nn.init.xavier_normal_(self.saliency_network_TEM.conv1.weight)
-        #torch.nn.init.xavier_normal_(self.saliency_network_TEM.conv2.weight)
         self.scanpath_network = scanpath_network
         self.fixation_selection_network = fixation_selection_network
         torch.nn.init.xavier_normal_(self.fixation_selection_network.conv1.weight)
         torch.nn.init.xavier_normal_(self.fixation_selection_network.conv2.weight)
-        print(saliency_network,conv_all_parameters,'objects')
-        self.fixation_selection_network_TEM = fixation_selection_network_TEM
-        self.conv_all_parameters =  conv_all_parameters
         self.conv_all_parameters_trans =  conv_all_parameters_trans
-        #self.batch_norm1=nn.BatchNorm2d(100)
-        #self.batch_norm2=nn.BatchNorm2d(100)
-        #self.batch_norm3=nn.BatchNorm2d(1)
-        #self.batch_norm4=nn.BatchNorm2d(1)
-        #self.dropout1 = nn.Dropout2d(0.3)
-        #self.dropout2 = nn.Dropout2d(0.3)
-        #self.dropout3 = nn.Dropout2d(0.15)
-        self.dropout5 = nn.Dropout2d(0.15)
-        #self.dropout4 = nn.Dropout2d(0.15)
-        #self.max_pool1 = nn.MaxPool2d((1,1))
-        #self.max_pool2 = nn.MaxPool2d((1,1))
-        torch.nn.init.xavier_normal_(self.conv_all_parameters.conv0.weight)
         torch.nn.init.xavier_normal_(self.conv_all_parameters_trans.conv0.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_p.conv_part0.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_p.conv_part1.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_p.conv_part2.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_part3.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_part4.weight)
-        #torch.nn.init.xavier_normal_(self.conv_all_parameters.conv_part5.weight)
-        #self.finalizer = Finalizer_TEM(
-        #    sigma=initial_sigma,
-        #    learn_sigma=True,
-        #    saliency_map_factor=self.saliency_map_factor,
-        #)
+        self.conv_all_parameters =  conv_all_parameters
+        torch.nn.init.xavier_normal_(self.conv_all_parameters.conv0.weight)
+        self.batch_norm3=nn.BatchNorm2d(1)
+        self.linear = nn.Linear(600,300)
+        torch.nn.init.xavier_normal_(self.linear.weight)
+        self.dropout5 = nn.Dropout2d(0.18)
 
         self.finalizer = Finalizer(
             sigma=initial_sigma,
@@ -470,10 +412,10 @@ class DeepGaze_TEM(torch.nn.Module):
         x=x.type('torch.cuda.FloatTensor')
         x = F.interpolate(x, scale_factor=1 / self.downsample)
         salv = []
+        
         for k in range(0,x.shape[0]):
-              TEM[k,0:3,:,:] = 255*((TEM[k,0:3,:,:]-torch.min(TEM[k,0:3,:,:]))/(torch.max(TEM[k,0:3,:,:])-torch.min(TEM[k,0:3,:,:])))
-              #TEM[k,0:300,:,:] = 255*((TEM[k,0:300,:,:]-torch.min(TEM[k,0:300,:,:]))/(torch.max(TEM[k,0:300,:,:])-torch.min(TEM[k,0:300,:,:])))
-              #TEM[k,300:303,:,:] = 255*((TEM[k,300:303,:,:]-torch.min(TEM[k,300:303,:,:]))/(torch.max(TEM[k,300:303,:,:])-torch.min(TEM[k,300:303,:,:])))
+              TEM[k,0:21,:,:] = 255*((TEM[k,0:21,:,:]-torch.min(TEM[k,0:21,:,:]))/(torch.max(TEM[k,0:21,:,:])-torch.min(TEM[k,0:21,:,:])))
+        
         x = self.features(x)
         readout_shape = [math.ceil(orig_shape[2] / self.downsample / self.readout_factor), math.ceil(orig_shape[3] / self.downsample / self.readout_factor)]
         
@@ -481,43 +423,17 @@ class DeepGaze_TEM(torch.nn.Module):
         
         TEM=F.interpolate(TEM,size=readout_shape,mode="bilinear")
         
-        for k in range(0,len(salv)):
-            for p in range(0,512):
-                x[k][:,p,:,:]=x[k][:,p,:,:]+TEM[:,0:1,:,:]
-        
         salv=x
-         
-        #x.append(TEM)
-
-        #sal1 = self.conv_all_parameters(TEM)
-        #sal1 = self.batch_norm1(sal1)
-        #sal1 = self.dropout1(sal1)
-        #sal1 = F.softplus(sal1)
-        #sal1 = self.max_pool1(sal1)
-        #sal1 = self.dropout1(sal1)
         
-        #sal2 = self.conv_all_parameters_trans(TEM)
-        #sal2 = self.batch_norm2(sal2)
-        #sal2 = self.dropout2(sal2)
-        #sal2 = F.softplus(sal2)
-        #sal2 = self.max_pool2(sal2)
-        #x = F.max_unpool2d(x,1)
-        #sal2 = self.dropout2(sal2)
+        salv.append(sal1)
+        salv.append(sal2)
+        salv.append(saln)
+        salv.append(TEM[:,:,:,:])
         
         salv = torch.cat(salv, dim=1)
         
         x = self.saliency_network(salv)
-        #x = self.batch_norm3(x)
-        #x = self.dropout3(x)
-        
-        #xt = self.saliency_network_TEM(torch.cat((TEM,sal1,sal2),dim=1))
-        #xt = self.saliency_network_TEM(TEM)
-        #xt = self.batch_norm4(xt)
-        #xt = self.dropout4(xt)
-        #x = self.saliency_network(x.cuda())
-        #x = self.fc(torch.cat((torch.flatten(x,start_dim=1),torch.flatten(TEM[:,0,:,:],start_dim=1)),dim=1))
-        #x = F.softplus(x)
-        #x = torch.reshape(x,(x.shape[0],1,80,107))
+        x = self.batch_norm3(x)
         
         if self.scanpath_network is not None:
             scanpath_features = encode_scanpath_features(x_hist, y_hist, size=(orig_shape[2], orig_shape[3]), device=x.device)
@@ -525,29 +441,18 @@ class DeepGaze_TEM(torch.nn.Module):
             y = self.scanpath_network(scanpath_features)
         else:
             y = None
-
-        x = self.fixation_selection_network((x,TEM[:,0:1,:,:],y))
+           
+        x = self.fixation_selection_network((x,y))
         x = self.dropout5(x)
         x = self.finalizer(x.type('torch.cuda.FloatTensor'), centerbias.type('torch.cuda.FloatTensor'))
-        #x = self.finalizer(x.type('torch.cuda.FloatTensor'),salv.type('torch.cuda.FloatTensor'),centerbias.type('torch.cuda.FloatTensor'),centerbias_TEM.type('torch.cuda.FloatTensor'))
         return x
 
     def train(self, mode=True):
         self.features.eval()
-        #self.conv_all_parameters.train(mode=mode)
-        #self.conv_all_parameters_trans.train(mode=mode)
-        #self.batch_norm1.train(mode=mode)
-        #self.dropout1.train(mode=mode)
-        #self.batch_norm2.train(mode=mode)
-        #self.dropout2.train(mode=mode)
-        #self.max_pool1.train(mode=mode)
-        #self.max_pool2.train(mode=mode)
+        self.conv_all_parameters.train(mode=mode)
+        self.conv_all_parameters_trans.train(mode=mode)
         self.saliency_network.train(mode=mode)
-        #self.batch_norm3.train(mode=mode)
-        #self.dropout3.train(mode=mode)
-        #self.saliency_network_TEM.train(mode=mode)
-        #self.batch_norm4.train(mode=mode)
-        #self.dropout4.train(mode=mode)
+        self.batch_norm3.train(mode=mode)
         if self.scanpath_network is not None:
             self.scanpath_network.train(mode=mode)
         self.fixation_selection_network.train(mode=mode)
