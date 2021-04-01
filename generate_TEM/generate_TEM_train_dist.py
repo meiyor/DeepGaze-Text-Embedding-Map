e405acf46d28f81cfc4d80b16aa943f1de78e4a0
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2021-04-01 16:39:19 -0500

    Update generate_TEM_train_dist.py

--- generate_TEM/generate_TEM_train_dist.py
+++ generate_TEM/generate_TEM_train_dist.py
@@ -107,6 +107,10 @@ for file_name in onlyfiles_train:
               ind_val=5
            elif cat_val=='office':
               ind_val=6
+           elif cat_val=='tennis_court':
+              ind_val=7
+           elif cat_val=='restaurant':
+              ind_val=8
            else:
               ind_val=7
            break
