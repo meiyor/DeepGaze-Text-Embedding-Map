3d6ce4520a41524d9dbadeb000e7f3390e61e509
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2021-10-08 16:02:51 +0100

    Update create_centerbias_TEM.py

--- create_centerbias_TEM.py
+++ create_centerbias_TEM.py
@@ -8,8 +8,8 @@ fixations = pysaliency.datasets.read_hdf5('fixations_train.hdf5')
 center_bias_corrected=BaselineModel(stimuli=stimuli,fixations=fixations,bandwidth=10**-1.6715425643341146,eps=10**-3.619089786523399)
 pysaliency.precomputed_models.export_model_to_hdf5(center_bias_corrected,stimuli,filename='centerbias_train_TEM.hdf5')
 
-stimuli = pysaliency.datasets.read_hdf5('/scratch/c.sapjm10/deepgaze_master_Evaluation/stimuli_val_TEM.hdf5')
-fixations = pysaliency.datasets.read_hdf5('/scratch/c.sapjm10/deepgaze_master_Evaluation/fixations_val.hdf5')
+stimuli = pysaliency.datasets.read_hdf5('/folder_stimuli/stimuli_val_TEM.hdf5')
+fixations = pysaliency.datasets.read_hdf5('/folder_fixation/fixations_val.hdf5')
 
 center_bias_corrected=BaselineModel(stimuli=stimuli,fixations=fixations,bandwidth=10**-1.6715425643341146,eps=10**-3.619089786523399)
-pysaliency.precomputed_models.export_model_to_hdf5(center_bias_corrected,stimuli,filename='/scratch/c.sapjm10/deepgaze_master_Evaluation/centerbias_val_TEM.hdf5')
+pysaliency.precomputed_models.export_model_to_hdf5(center_bias_corrected,stimuli,filename='centerbias_val_TEM.hdf5')
