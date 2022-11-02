92bc412048e4a76b3f21399c7e6589b7b9307101
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2022-11-02 16:31:00 +0000

    Update eval_stats_paired.m

--- matlab_metrics/eval_stats_paired.m
+++ matlab_metrics/eval_stats_paired.m
@@ -3,4 +3,5 @@ P(1,:)=stats_paired_tests(Data.valbaseline_IG,Data.valscanpath_IG);
 P(2,:)=stats_paired_tests(Data.valbaseline_AUC,Data.valscanpath_AUC);
 P(3,:)=stats_paired_tests(Data.valbaseline_NSS,Data.valscanpath_NSS);
 P(4,:)=stats_paired_tests(Data.valbaseline_LL,Data.valscanpath_LL);
+%% bonferroni holm correction for each method evaluation
 Pnew=bonf_holm(P);
